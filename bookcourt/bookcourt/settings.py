c7e44983fea003cb75c220fcf7983ff9c8ce31e1
SeveralSnipe <126894004+SeveralSnipe@users.noreply.github.com>
2023-10-24 21:02:38 +0530

    Update settings.py

--- bookcourt/bookcourt/settings.py
+++ bookcourt/bookcourt/settings.py
@@ -146,4 +146,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'testgamefront@gmail.com'
-EMAIL_HOST_PASSWORD = 'iccviintoqqnanyq'
+EMAIL_HOST_PASSWORD = # it's a secret :)
